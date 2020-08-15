9756fcf0c936762bfc9fd140a53b87734ce33f2e
kusum <mohammedkusum3@gmail.com>
2020-08-15 11:20:20 +0000

    week three's assignment

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+const Header =()=>{
+    return(
+<div>
+    {/*.NAV.BAR*/}
+    <nav class="navbar">
+      <div class="container">
+        <a href="#">Hulk</a>
+        {/*.<!--.Currently.Active.-->.*/}
+        <a href="#" class="logo is-active">Mavel</a>
+        <a href="#">Venom</a>
+      </div>
+    </nav>
+
+</div>
+
+    )
+}
+
+export default Header;
